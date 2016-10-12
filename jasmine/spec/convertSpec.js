f61e2a8bd3e0630711b3a29a2684b153af89c7ec
Ravi Singh <maxsteel.ravi26@gmail.com>
2016-10-12 08:09:29 +0530

    trying jasmine framework for testcase

---
+++ jasmine/spec/convertSpec.js
@@ -0,0 +1,10 @@
+describe( "Check login page", function () {
+    it("login page test", function () {
+        expect(Convert(12, "in").to("cm")).toEqual(30.48);
+    });
+
+    it("signup page", function () {
+        expect(Convert(2000, "cm").to("yards")).toEqual(21.87);
+    });
+});
+
